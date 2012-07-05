8f126c8ea11f2a4d7d5874142d867acdd97d6f71
Nick Pope <nick@nickpope.me.uk>
2012-07-05 23:42:48 +0100

    Various tweaks/fixes to tests.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -13,7 +13,6 @@ mb_internal_encoding('UTF-8');
 # Set up path variables.
 $ROOT = dirname(dirname(__FILE__));
 $DATA = $ROOT.'/tests/data/twitter-text-conformance';
-define('DATA', $DATA);
 
 # Include required classes.
 require_once $ROOT.'/lib/Twitter/Autolink.php';
