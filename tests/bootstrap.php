ce76b045d1a2841277c5de6aa03fe0c6523dc674
nojimage <nojimage@gmail.com>
2014-03-16 21:10:21 +0900

    fixes tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,9 +1,6 @@
 <?php
 
 # Set up error reporting:
-if (!defined('E_DEPRECATED')) {
-    define('E_DEPRECATED', 8192);
-}
 error_reporting(E_ALL | E_STRICT | E_DEPRECATED);
 
 # Set default timezone to hide warnings:
@@ -13,11 +10,10 @@ date_default_timezone_set('Europe/London');
 mb_internal_encoding('UTF-8');
 
 # Set up path variables.
-$ROOT = dirname(dirname(__FILE__));
-$DATA = $ROOT . '/vendor/twitter/twitter-text-conformance';
+$DATA = __DIR__ . '/twitter-text-conformance';
 if (!defined('DATA')) {
     define('DATA', $DATA);
 }
 
 # Include required classes.
-require $ROOT . '/vendor/autoload.php';
+require dirname(__DIR__) . '/vendor/autoload.php';
--- tests/runtests.php
+++ tests/runtests.php
@@ -11,128 +11,12 @@
  * @license    http://www.apache.org/licenses/LICENSE-2.0  Apache License v2.0
  */
 require_once dirname(__FILE__) . '/bootstrap.php';
+require_once dirname(__FILE__) . '/util.php';
 
 use Symfony\Component\Yaml\Yaml;
 
 $browser = (PHP_SAPI != 'cli');
 
-/**
- *
- */
-function pretty_format($a)
-{
-    if (is_bool($a)) {
-        return $a ? 'true' : 'false';
-    }
-    if (is_string($a)) {
-        return "'${a}'";
-    }
-    return preg_replace(array(
-        "/\n/", '/ +\[/', '/ +\)/', '/Array +\(/', '/(?<!\() \[/', '/\[([^]]+)\]/',
-        '/"(\d+)"/', '/(?<=^| )\((?= )/', '/(?<= )\)(?=$| )/',
-        ), array(
-        ' ', ' [', ' )', '(', ', [', '"$1"', '$1', '[', ']',
-        ), print_r($a, true));
-}
-
-/**
- *
- */
-function output_preamble()
-{
-    global $browser;
-    if (!$browser) {
-        return;
-    }
-    echo <<<EOHTML
-<!DOCTYPE html>
-<html lang="en-GB">
-<head>
-<meta charset="utf-8">
-<title>Twitter Text (PHP Edition) Library » Conformance</title>
-<style>
-body { font-family: sans-serif; font-size: 12px; }
-.pass { color: #090; }
-.fail { color: #f00; }
-</style>
-</head>
-<body>
-EOHTML;
-}
-
-/**
- *
- */
-function output_closing()
-{
-    global $browser;
-    if (!$browser) {
-        return;
-    }
-    echo <<<EOHTML
-</body>
-</html>
-EOHTML;
-}
-
-/**
- *
- */
-function output_h1($text)
-{
-    global $browser;
-    if ($browser) {
-        echo '<h1>' . $text . '</h1>';
-    } else {
-        echo "\033[1m" . $text . "\033[0m" . PHP_EOL;
-        echo str_repeat('=', mb_strlen($text)) . PHP_EOL;
-    }
-    echo PHP_EOL;
-}
-
-/**
- *
- */
-function output_h2($text)
-{
-    global $browser;
-    if ($browser) {
-        echo '<h2>' . $text . '</h2>';
-    } else {
-        echo "\033[1m" . $text . "\033[0m" . PHP_EOL;
-        echo str_repeat('-', mb_strlen($text)) . PHP_EOL;
-    }
-    echo PHP_EOL;
-}
-
-/**
- *
- */
-function output_h3($text)
-{
-    global $browser;
-    if ($browser) {
-        echo '<h3>' . $text . '</h3>';
-    } else {
-        echo "\033[1m" . $text . "\033[0m" . PHP_EOL;
-    }
-    echo PHP_EOL;
-}
-
-/**
- *
- */
-function output_skip_test()
-{
-    global $browser;
-    $text = 'Skipping Test...';
-    if ($browser) {
-        echo '<p>' . $text . '</p>';
-    } else {
-        echo "   \033[1;35m" . $text . "\033[0m" . PHP_EOL;
-    }
-    echo PHP_EOL;
-}
 $pass_total = 0;
 $fail_total = 0;
 $pass_group = 0;
