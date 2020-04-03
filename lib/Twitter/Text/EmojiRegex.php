a502ced08cfe6c6af0f937c9c51ee6fb5fe9fdb7
nojimage <nojimage@gmail.com>
2020-04-03 10:21:31 +0900

    Update link in doc comment

--- lib/Twitter/Text/EmojiRegex.php
+++ lib/Twitter/Text/EmojiRegex.php
@@ -12,7 +12,7 @@ namespace Twitter\Text;
  * EmojiRegex
  *
  * @link http://unicode.org/Public/emoji/12.1/
- * @link https://unicode.org/emoji/charts/full-emoji-list.html
+ * @link https://unicode.org/emoji/charts-12.1/full-emoji-list.html
  */
 class EmojiRegex
 {
