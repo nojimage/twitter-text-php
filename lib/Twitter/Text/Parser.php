4cb77d65bbadbfda280eec423b65e37491f77f4d
nojimage <nojimage@gmail.com>
2020-04-03 09:55:36 +0900

    Update twitter-text to 3.1.0
    - Update tld matching pattern
    - Fixes emoji weight

--- lib/Twitter/Text/Parser.php
+++ lib/Twitter/Text/Parser.php
@@ -96,7 +96,7 @@ class Parser
                 $emojiLength = StringUtils::strlen($emoji);
                 $charCount = StringUtils::charCount($emoji);
 
-                $weightedCount += $this->getCharacterWeight(StringUtils::substr($emoji, 0, 1), $this->config);
+                $weightedCount += $this->config->defaultWeight;
                 $offset += $emojiLength;
                 $displayOffset += $charCount;
                 if ($weightedCount <= $maxWeightedTweetLength) {
